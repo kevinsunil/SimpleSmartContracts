2d9e2612e11a820842e414eb3d1310d59140c765
kevinsunil <kevinsunil.2001@gmail.com>
2022-03-11 23:52:29 +0530

    .

---
+++ Voting.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+contract Ballot{
+    //VARIABLES
+    struct vote{
+        address voterAddress;
+        bool choice;
+    }
+    struct voter{
+        string voterName;
+        bool voted;
+    }
+
+    uint private countResult =0;
+    uint public finalResult = 0;
+
+    //MODIFIERS
+
+    //EVENTS
+
+    //FUNCTIONS
+}
